074964a61eb6ff406293a1f9f57ef5ee4f68623d
shlok1999 <09shlok1999ae1@gmail.com>
2022-02-14 15:46:32 +0530

    made the navbar responsive

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let menu_icon = document.querySelector('.menu-icon')
-let nav_services = document.querySelector('.right-nav-services')
-menu_icon.addEventListener('click', (e)=>{
-   
-})
+// let menu_bar = document.querySelector(".menu-icon")
+function isNav(e){
+    var x = document.getElementById('right-nav-responsive')
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+      } else {
+        x.style.display = "none";
+      }
+    
+}
